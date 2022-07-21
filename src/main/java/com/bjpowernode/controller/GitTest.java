ae850df0f9fca31a0e7d54852282359b90a60746
wangpeng <wangpeng@zbitedu.cn>
2022-07-21 09:18:24 +0800

    fifth commit

--- src/main/java/com/bjpowernode/controller/GitTest.java
+++ src/main/java/com/bjpowernode/controller/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git2");
         System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
